bccfcf2658aa44d7e71e0ef83dc18acd10b8f407
seeyang1 <51054781+seeyang1@users.noreply.github.com>
2022-02-24 23:11:12 -0500

    Add files via upload

--- sifter.py
+++ sifter.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python3
 
-#sudo code:
-#open pcap file
-#create a new file, B, to store the new data in
-#while loop to read every line
-#tons of if statements that find the text we need
-#if statements will write the data we need into file B
-#after if statement is done CLOSE THE FILE!!!!!
-#close file B 
-#be on with your day
-
-f = open("abc.txt", "r")
+source=[]
+destination=[]
+temp=""
+temp2=""
+pFlag = False
+iFlag = False
+carrotFlag = False
+f = open("testLine.txt", "r")
 #data = open("data.txt","w")
-while f:  
-    line = f.readline()
-    #sort through the line and extract source and destination IPs
-
+for line in f:
+    for character in line:
+        if character == "I":
+            iFlag = True
+        if character == "P":
+            pFlag = True
+        if character ==">":
+            pFlag = False
+            iFlag = False
+            carrotFlag = True
+        if character == ":":
+            carrotFlag = False
+        if pFlag and iFlag:
+            temp = temp+character
+        if carrotFlag:
+            temp2 = temp2+character
+    temp=temp.lstrip("P ")
+    temp2=temp2.lstrip("> ")
+    #print(temp)
+    #print(temp2)
+    source.append(temp)
+    destination.append(temp2)
+    temp=""
+    temp2=""
+print("source:" , source)
+print("destination:",destination)
 f.close();
-
