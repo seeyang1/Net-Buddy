e3131c08069279e436686c63b0e0059f492be1b4
seeyang1 <51054781+seeyang1@users.noreply.github.com>
2022-04-10 16:07:33 -0400

    Add files via upload

--- sifter.py
+++ sifter.py
@@ -1,37 +1,32 @@
 #!/usr/bin/env python3
+import sys
+import re
+import time
 
+start_time = time.time()
+file = str(sys.argv[1])
+f = open(file, "r")
 packets=[]
-temp=""
-temp2=""
-pFlag = False
-iFlag = False
-carrotFlag = False
-f = open("testLine.txt", "r")
 for line in f:
-    for character in line:
-        if character == "I":
-            iFlag = True
-        if character == "P":
-            pFlag = True
-        if character ==">":
-            pFlag = False
-            iFlag = False
-            carrotFlag = True
-        if character == ":":
-            carrotFlag = False
-        if pFlag and iFlag:
-            temp = temp+character
-        if carrotFlag:
-            temp2 = temp2+character
-    temp=temp.lstrip("P ")
-    temp2=temp2.lstrip("> ")
-    packets.append((temp,temp2))
-    temp=""
-    temp2=""
-print("packet tuples:" , packets)
+    x = re.split("\s", line)
+    source = x[2]
+    destination = x[4]
+    protocol = x[5]
+    if re.search("10.0.2.15", source):
+        #print('found home ip',source)
+        y = re.split('\.',source)
+        source = y[0]+'.'+y[1]+'.'+y[2]+'.'+y[3]
+        port = y[4]
+        z = re.split('\.',destination)
+        destination = z[0]+'.'+z[1]+'.'+z[2]+'.'+z[3]
+    else:
+        y = re.split('\.',destination)
+        destination = y[0]+'.'+y[1]+'.'+y[2]+'.'+y[3]
+        port = y[4]
+        z = re.split('\.',source)
+        source = z[0]+'.'+z[1]+'.'+z[2]+'.'+z[3]
+    #print(source, destination, protocol)
+    packets.append((source, destination, protocol, port))
+print(packets)
+print("--- %s seconds ---" % (time.time()-start_time))
 f.close();
-
-
-#notes and to do list:
-#obug(?) where last element in the list of tuples is empty. This occurs in every file we check
-#should look at having the script take as input a txt file so that file being read isn't hard coded
