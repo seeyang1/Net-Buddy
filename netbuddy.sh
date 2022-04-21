c12ecc1cd5b637407ea0a9288a8f9ccfa25ddad0
seeyang1 <51054781+seeyang1@users.noreply.github.com>
2022-04-21 15:51:09 -0400

    Add files via upload

--- netbuddy.sh
+++ netbuddy.sh
@@ -1,3 +1,19 @@
-sudo tcpdump -i eth0 -n > sniffed.txt;
+#!/bin/bash
+sudo tcpdump -i eth0 -n -q > sniffed.txt &
+sleep 30 
+pid=$(ps -e | pgrep tcpdump)
+echo $pid
+sleep 1
+sudo kill -2 $pid
 head -n -1 sniffed.txt > sniff.txt
-./sifter.py sniff.txt;
+while :
+do
+	sudo tcpdump -i eth0 -n -q > sniffed.txt &
+	./sifter.py sniff.txt
+	sleep 30
+	pid=$(ps -e | pgrep tcpdump)
+	echo $pid
+	sleep 1
+	sudo kill -2 $pid
+	head -n -1 sniffed.txt > sniff.txt
+done
