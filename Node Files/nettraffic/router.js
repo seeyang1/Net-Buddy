9430dd3e3a356e5fb84c9ab299bac540c5012425
Nickolas Dimitrakas <60496866+nikolakis321@users.noreply.github.com>
2022-05-05 03:08:47 -0400

    Delete router.js

--- Node Files/nettraffic/router.js
+++
@@ -1,7 +0,0 @@
-const Router = require('@koa/router');
-const router = new Router();
-const nettrafficController = require('./controller');
-
-router.get('/', nettrafficController.get);
-
-module.exports = router;
